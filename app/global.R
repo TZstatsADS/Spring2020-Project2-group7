ba0fa3c7d66a32fa3b282c0962eed5d983a74527
YuqLiu <60188955+YuqLiu@users.noreply.github.com>
2020-02-20 17:50:17 -0500

    Update global.R

--- app/global.R
+++ app/global.R
@@ -10,47 +10,47 @@ library(magrittr)
 
 load("../output/Econ_state_map.RData")
 Econ_data_state <- Econ_data_state %>% mutate(Name = str_to_upper(Name))
+
 mapStates = maps::map("state", fill = TRUE, plot = FALSE)
 names <- tibble(Name=str_to_upper(mapStates$names)) %>% 
   separate(Name, c('Name', 'sub'), ':') %>% 
   select(Name)
 
-state_map <- function(df, values){
-  # values <- c(unlist(df$Value))
-  # pal <- colorBin("plasma", domain = values)
-  # labels <- sprintf(
-  #   "<strong>%s</strong><br/>%g",
-  #   df$Name,values) %>%
-  #   lapply(htmltools::HTML)
-  #pal <- colorBin("plasma", domain = values)
-  # State Map
+state_map <- function(values){
+  pal <- colorBin("plasma", domain = values)
+  labels <- sprintf(
+    "<strong>%s</strong><br/>%g",
+    names$Name, values) %>%
+    lapply(htmltools::HTML)
+
+  #State Map
   leaflet(data = mapStates) %>%  
     setView(-96, 37.8, 4.3) %>%
     addTiles() %>%
     addResetMapButton() %>% 
     addPolygons(
-      #fillColor = pal(values),
+      fillColor = pal(values),
       weight = 2,
       opacity = 1,
       color='white',
       dashArray = 3,
       fillOpacity = .7,
       highlight = highlightOptions(
-        weight = 5,
+        weight = 4,
         color = "#666",
         dashArray = "",
         fillOpacity = .75,
         bringToFront = T
-      ))
-    #   label = labels,
-    #   labelOptions = labelOptions(
-    #     style = list("font-weight" = "normal", 'padding' = "10px 15px"),
-    #     textsize = "15px",
-    #     direction = "auto")
-    #   ) %>%
-    # addLegend(pal = pal,
-    #         values = values,
-    #         opacity = 0.85,
-    #         title = NULL,
-    #         position = "bottomright")
+      ),
+      label = labels,
+      labelOptions = labelOptions(
+        style = list("font-weight" = "normal", 'padding' = "10px 15px"),
+        textsize = "15px",
+        direction = "auto")
+      ) %>% 
+    addLegend(pal = pal,
+            values = values,
+            opacity = 0.85,
+            title = NULL,
+            position = "bottomright")
 }
