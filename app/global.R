a21fb784b84d1807e2d234a3afd9e15673b370c7
YuqLiu <60188955+YuqLiu@users.noreply.github.com>
2020-02-20 23:57:22 -0500

    Update global.R

--- app/global.R
+++ app/global.R
@@ -7,6 +7,8 @@ library(viridis)
 library(htmltools)
 library(leaflet.extras)
 library(magrittr)
+devtools::session_info()
+
 
 load("../output/Econ_state_map.RData")
 Econ_data_state <- Econ_data_state %>% mutate(Name = str_to_upper(Name))
@@ -16,11 +18,11 @@ names <- tibble(Name=str_to_upper(mapStates$names)) %>%
   separate(Name, c('Name', 'sub'), ':') %>% 
   select(Name)
 
-state_map <- function(values){
-  pal <- colorBin("plasma", domain = values)
+state_map <- function(df, input_metric){
+  pal <- colorBin("plasma", domain = df$Value)
   labels <- sprintf(
-    "<strong>%s</strong><br/>%g",
-    names$Name, values) %>%
+    "<strong>%s</strong><br/>%s</strong><p/>%.4g",
+    df$Name, input_metric, df$Value) %>%
     lapply(htmltools::HTML)
 
   #State Map
@@ -29,27 +31,26 @@ state_map <- function(values){
     addTiles() %>%
     addResetMapButton() %>% 
     addPolygons(
-      fillColor = pal(values),
+      fillColor = pal(df$Value),
       weight = 2,
       opacity = 1,
       color='white',
       dashArray = 3,
       fillOpacity = .7,
-      highlight = highlightOptions(
-        weight = 4,
-        color = "#666",
-        dashArray = "",
-        fillOpacity = .75,
-        bringToFront = T
-      ),
+      highlight = highlightOptions(weight = 4,
+                                   color = "#666",
+                                   dashArray = "",
+                                   fillOpacity = .75,
+                                   bringToFront = T
+                                   ), 
       label = labels,
       labelOptions = labelOptions(
         style = list("font-weight" = "normal", 'padding' = "10px 15px"),
         textsize = "15px",
         direction = "auto")
-      ) %>% 
+      ) %>%
     addLegend(pal = pal,
-            values = values,
+            values = df$Value,
             opacity = 0.85,
             title = NULL,
             position = "bottomright")
