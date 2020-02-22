e9422510be6670ea0a0f07f47a02b16aecac20aa
JinxuXiang <jx2399@columbia.edu>
2020-02-22 13:07:44 -0500

    update county

--- counties_map/global.R
+++ counties_map/global.R
@@ -11,10 +11,16 @@ library(magrittr)
 
 load("../output/Econ_county_map.RData")
 zoom = rep(7, length(state.abb))
-zoom[c(5,12,23,28,43)] = 6
-zoom[c(20,21,29,30,38,40,45,48)] = 8
-zoom[c(7,8)] = 9
-zoom[c(39)] = 10
+zoom[c(4)] = 6
+zoom[c(1,3,9,12,13,14,15,16,17,22,23,25,27,31,32,33,34,36,38,39,40,44,46)] = 8
+zoom[c(6,7,18,19,28,43)] = 9
+zoom[c(37)] = 10
+
+
+state_choise = state.abb
+names(state_choise) = state.name
+state_choise = c(state_choise[1],state_choise[3:10], state_choise[12:length(state_choise)])
+
 
 county_map <- function(df){
   mapcounty <- maps::map("county", regions = state.name[which(state.abb == df$State[1])], fill = TRUE, plot = FALSE)
@@ -23,23 +29,24 @@ county_map <- function(df){
     mutate(state = paste0(toupper(substr(state,1,1)), substr(state, 2, nchar(state)))) %>% 
     mutate(county = paste0(toupper(substr(county,1,1)), substr(county, 2, nchar(county)), ifelse(state == 'Louisiana', ' Parish', ' County')))
   
-state_choise = state.abb
-names(state_choise) = state.name
-state_choise = c(state_choise[1],state_choise[3:10], state_choise[12:length(state_choise)])
-  
-  
   df <- df %>% 
-    right_join(names, by = c('Name' = 'county'))
+    right_join(names, by = c('Name' = 'county')) %>% 
+    arrange(State, Year)
   values <- c(unlist(df[,4]))
   metric <- colnames(df)[4]
-  precentage <- df[1,5]
+  chs <- df[1,5]
   year <- df[1,3]
   state <- state.name[which(state.abb == df$State[1])]
   pal <- colorBin("YlOrRd", domain = values, bins = 9)
-  labels <- sprintf(
-    "<strong>%s, %s </strong><br/><strong>%s<br/>in %s</strong><br/>%g",    
-    state, df$Name, metric, year, values)
-  if(precentage == 1)
+  if(chs == 1)
+    labels <- sprintf(
+      "<strong>%s, %s </strong><br/><strong>%s<br/>in %s</strong><br/>%g",    
+      state, df$Name, metric, year, values)
+  else
+    labels <- sprintf(
+      "<strong>%s, %s </strong><br/><strong>%s<br/>from %s</strong><br/>%g",    
+      state, df$Name, metric, year, values)
+  if(chs == 3)
     labels <- paste0(labels, '%')
   labels = labels%>% 
     lapply(htmltools::HTML)
@@ -48,7 +55,7 @@ state_choise = c(state_choise[1],state_choise[3:10], state_choise[12:length(stat
   
   # County Map
   leaflet(data = mapcounty) %>% 
-    setView(mean(range[1:2]), mean(range[3:4]), zoom[which(state.abb == df$State[1])]) %>% 
+    setView(mean(range[1:2]), mean(range[3:4]), zoom[which(state_choise== df$State[1])]) %>% 
     addTiles() %>%
     addResetMapButton() %>% 
     addPolygons(
