ebbff22d63e7069ff83e0bf98c20a48e13ea3272
YuqLiu <60188955+YuqLiu@users.noreply.github.com>
2020-02-24 11:34:52 -0500

    Update global.R

--- app/global.R
+++ app/global.R
@@ -170,6 +170,7 @@ unit_lyq <- function(input_metric, chs){
   
 ###########################################################################################
 ##Specific Stats
+theme_set(theme_light())
 index1<-function(df){
   df[,3]%>%unlist()
 }
